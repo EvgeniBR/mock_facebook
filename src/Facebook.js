484267f707b99d887df49b5f86875300160122a8
zrihe <zrihend@gmail.com>
2020-11-01 16:16:38 +0200

    remove stater file and add router to register page

---
+++ src/Facebook.js
@@ -0,0 +1,22 @@
+import React , {useCallback} from "react";
+import { useHistory } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./login-page/Login";
+
+const Facebook = () => {
+  let history = useHistory();
+
+
+
+  return (
+    <Router>
+      <Switch>
+        {/* <Route exact path="/" render={(props) => <App {...props} />} /> */}
+        <Route path="/register" render={(props) => <Login {...props} />} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default Facebook;
---
+++ src/login-page/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="Login">
+        yo
+    </div>
+  );
+}
+
+export default Login;
