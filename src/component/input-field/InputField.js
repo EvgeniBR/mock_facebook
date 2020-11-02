fe56e5212b69ac866bb7f1cdc29737bcc914f916
zrihe <zrihend@gmail.com>
2020-11-02 09:33:00 +0200

    update sign in

--- src/component/input-field/InputField.js
+++ src/component/input-field/InputField.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InputField.css";
 
-const InputField = ({Inputtype , textOfPlaceholder}) => {
+const InputField = ({ Inputtype, textOfPlaceholder }) => {
+  const [inputValue, setInputValue] = useState("");
   return (
     <input
       className="InputField"
       type={Inputtype}
       placeholder={textOfPlaceholder}
+      value={inputValue}
+      onChange={(e) => {
+        setInputValue(e.target.value);
+        //לשרשר את הvalue
+      }}
     />
   );
 };
