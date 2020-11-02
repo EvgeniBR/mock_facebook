b1f61f6f7a973a98f6ce28f82911b025d6fa165d
EvgeniBR <genib1989@gmail.com>
2020-11-02 14:04:28 +0200

    status change from 400 to 200

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -21,7 +21,7 @@ router.post('/users/login', async (req, res) => {
         const token = await user.generateAuthToken()
         res.send({ user, token })
     } catch (e) {
-        res.status(400).send(e)
+        res.status(200).send( {'error': 'Invalid mail or password' })
         console.log('i');
     }
 })
