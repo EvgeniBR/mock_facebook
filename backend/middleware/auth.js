e08102b236d53b1e72ec826de7d7f1eb10c82ab9
EvgeniBR <genib1989@gmail.com>
2020-11-03 14:10:07 +0200

    backend-user login&logout commit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,21 +4,21 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        console.log(token);
+        
         const decoded = jwt.verify(token, 'facebookmock')
-        console.log(decoded);
-        console.log('try');
+        
+        
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
         if (!user) {
-            console.log('if');
+            
             throw new Error()
         }
-
+        req.token = token
         req.user = user
         next()
     } catch (e) {
         res.status(401).send({ error: 'Please authenticate.' })
-        console.log('catch');
+        
     }
 }
 
