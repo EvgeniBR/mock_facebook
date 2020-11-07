423f123a6ab10b83a96e7e0d5caf652a5185679b
EvgeniBR <genib1989@gmail.com>
2020-11-07 16:30:56 +0200

    profile

--- client/src/component/profile-render/ProfileRender.js
+++ client/src/component/profile-render/ProfileRender.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Profile from '../../profile-page/Profile'
 import FriendProfile from '../../friend-profile-page copy/FriendProfile'
-
+import DataService from '../../db-connection/DataService'
+import Cookies from 'universal-cookie'
 
 const ProfileRender = () => {
+
+    const cookies = new Cookies();
+
+    useEffect(()=>{
+        const token = cookies.get('mockFacebookToken');
+
+        if(!token){
+            return 'no token'
+        }
+        const getData = async() =>{
+            const user = await DataService.get('users/me' , {
+                headers:{
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            console.log(user);
+        }
+        getData()
+    },[])
 
     
   return (
