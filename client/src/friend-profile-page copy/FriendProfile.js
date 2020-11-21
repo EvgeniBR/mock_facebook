24fdd4c984c48d520754deda354a29305c1bddaa
EvgeniBR <genib1989@gmail.com>
2020-11-21 23:36:51 +0200

    abc

--- client/src/friend-profile-page copy/FriendProfile.js
+++ client/src/friend-profile-page copy/FriendProfile.js
@@ -1,10 +1,20 @@
-import { request } from "http";
-import React  from "react";
+import React,{useEffect,useState }  from "react";
 import FriendCover from "./FriendCover";
 import FriendProfileHeader from "./FriendProfileHeader";
+import PostContainer from "../component/post-container/PostContainer";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../theme";
+import { GlobalStyles } from "../global";
 
 
 const FriendProfile = (props) => {
+  const [userName, setUserName] = useState("");
+  const [userLastName, setUserLastName] = useState("");
+  const [userPath, setUserPath] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
+  // const [friendRequests, setFriendRequests] = useState("");
+  const [theme , setTheme] = useState(darkTheme);
+
   console.log('f' , props);
 
   let friendRequest = false;
@@ -15,13 +25,33 @@ const FriendProfile = (props) => {
     friendRequest = findRequest;
   }
 
+  useEffect(() => {
+      
+      setUserName(props.data.data.first_name);
+      setUserLastName(props.data.data.last_name);
+      setUserPath(props.data.data.path);
+      setProfilePicture(props.data.data.avatar);
+      // setFriendRequests(user.data.friendsRequest);
+  
+    // false ? setTheme(lightTheme) : setTheme(darkTheme)
+  // eslint-disable-next-line
+  }, []);
 
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+    <GlobalStyles />
       <FriendCover data = {props.data} />
       <br/>
      <FriendProfileHeader userPath={props.userPath} profilePath={props.data.data.path} request={friendRequest}/>
-    </div>
+     <PostContainer
+        // writePost={() => setWritePostMode(true)}
+        profileAvatar={profilePicture}
+        firstName={userName}
+        lastName={userLastName}
+        userPath={userPath}
+        theme = {theme}
+      />
+     </ThemeProvider>
   );
 };
 
