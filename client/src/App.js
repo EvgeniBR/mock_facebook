c59278322f15bd62d5d2df97141bfd06f4deb933
dorin zrihen <zrihend@gmail.com>
2020-11-26 09:39:44 +0200

    svg feed page

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import "./App.css"
 
 function App() {
   return (
-    <div className="facebook">
-        <Facebook/>
+    <div className="facebook">  
+      <Facebook/> 
     </div>
   );
 }
--- client/src/feed-page/FacebookShortcut.js
+++ client/src/feed-page/FacebookShortcut.js
@@ -1,71 +1,71 @@
 import React from "react";
-import CircleDivWithIcon from "../component/circle-div/CircleDivWithIcon";
 
 const FacebookShortcut = ({color}) => {
+
   return (
     <div className="facebookShortcut" style={{color:color}}>
       <div>
-        <i className="fas fa-flag" style={{color:"orange"}}></i>
+        <i className="fas fa-flag flag"></i>
         <p>Pages</p>
       </div>
       <div>
-        <i className="fas fa-heartbeat" style={{color:"purple"}}></i>
+        <i className="fas fa-heartbeat covid"></i>
         <p>COVID-19 Information Center</p>
       </div>
       <div>
-        <i className="fas fa-user-friends" style={{color:"blue"}}></i>
+        <i className="fas fa-user-friends friends"></i>
         <p>Friends</p>
       </div>
       <div>
-        <i className="fas fa-users" style={{color:"blue"}}></i>
+        <i className="fas fa-users group"></i>
         <p>Groups</p>
       </div>
       <div>
-        <i className="fas fa-bullhorn" style={{color:"white"}}></i>
+        <i className="fas fa-bullhorn adCenter" style={{color:"white"}}></i>
         <p>Ad Center</p>
       </div>
       <div>
-        <i className="fas fa-chart-bar" style={{color:"blue"}}></i>
+        <i className="fas fa-chart-bar manager" style={{color:"blue"}}></i>
         <p>Ads Manager</p>
       </div>
       <div>
-        <i className="far fa-calendar" style={{color:"red"}}></i>
+        <i className="far fa-calendar events" style={{color:"red"}}></i>
         <p>Events</p>
       </div>
       <div>
-        <i className="fas fa-gamepad" style={{color:"blue"}}></i>
+        <i className="fas fa-gamepad games" style={{color:"blue"}}></i>
         <p>Games</p>
       </div>
       <div>
-        <i className="fas fa-clock" style={{color:"white"}}></i>
+        <i className="fas fa-clock memories" style={{color:"white"}}></i>
         <p>Memories</p>
       </div>
       <div>
-        <i className="fas fa-briefcase" style={{color:"orange"}}></i>
+        <i className="fas fa-briefcase jobs" style={{color:"orange"}}></i>
         <p>Jobs</p>
       </div>
       <div>
-        <i className="fab fa-facebook-messenger" style={{color:"purple"}}></i>
+        <i className="fab fa-facebook-messenger messenger" style={{color:"purple"}}></i>
         <p>Messenger</p>
       </div>
       <div>
-        <i className="fas fa-cloud-sun" style={{color:"yellow"}}></i>
-        <p>Offers</p>
+        <i className="fas fa-cloud-sun Weather" style={{color:"yellow"}}></i>
+        <p>Weather</p>
       </div>
       <div>
-        <i className="fab fa-youtube" style={{color:"blue"}}></i>
+        <i className="fab fa-youtube video" style={{color:"blue"}}></i>
         <p>Videos</p>
       </div>
       <div>
-        <i className="fas fa-clipboard-check" style={{color:"purple"}}></i>
+        <i className="fas fa-clipboard-check saved" style={{color:"purple"}}></i>
         <p>Saved</p>
       </div>
       <div>
-        <i className="fas fa-store-alt" style={{color:"blue"}}></i>
+        <i className="fas fa-store-alt marketplace" style={{color:"blue"}}></i>
         <p>Marketplace</p>
       </div>
       <div>
-        <i className="fab fa-facebook-messenger" style={{color:"purple"}}></i>
+        <i className="fab fa-facebook-messenger messenger" style={{color:"purple"}}></i>
         <p>Messenger</p>
       </div>
     </div>
